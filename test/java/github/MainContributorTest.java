e33b505d2dc2ab6b38aa9a157f9ce2bc46b0295e
Tatsiana Sakalinskaya <40210535+tani4ik@users.noreply.github.com>
2022-03-25 10:38:34 +0300

    Update MainContributorTest.java

--- test/java/github/MainContributorTest.java
+++ test/java/github/MainContributorTest.java
@@ -2,14 +2,14 @@ package github;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
 
 public class MainContributorTest {
 
   @Test
-  void andreiSolntsevShouldBeTheMainContributor() {
+  void checkMainContributor() {
     // открыть страничку репозитория Селенида
     open("https://github.com/selenide/selenide");
     // подвести мышку к первому элементу в области Contributors
@@ -17,7 +17,6 @@ public class MainContributorTest {
             .closest(".BorderGrid-row").$("ul li").hover();
 
     // check: в появившемся окошке (оверлей) текст Andrei Solntsev
-    $$(".Popover-message").findBy(visible).shouldHave(text("Andrei Solntsev"));
-
+    $$(".Popover-message").shouldHave(text("Andrei Solntsev"));
   }
 }
